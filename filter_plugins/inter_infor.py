de2cb6d3d31276e4f30b7f921edc4495de0b3712
likefour <54854200+likefour@users.noreply.github.com>
2023-03-15 14:23:05 +0800

    Update inter_infor.py

--- filter_plugins/inter_infor.py
+++ filter_plugins/inter_infor.py
@@ -1,11 +1,12 @@
 from ansible.plugins.filter.core import FilterModule
-# filter vlan information from show inter status cmd output
-class FilterModule(FilterModule):
+
+class FilterModule():
     def filters(self):
         return {
             # In the class, define a method called filters() that returns a dictionary of filter names and the functions they map to.
            # 'my_filter': self.my_filter 
-           'inter_infor': self.inter_infor
+           'inter_infor': self.inter_infor,
+           'inter_infor_vlan': self.inter_infor_vlan
         }
 
     def inter_infor(self, datas):
@@ -19,4 +20,12 @@ class FilterModule(FilterModule):
             'interface': inter_for
         }
               
+        return inter_for
+    
+    def inter_infor_vlan(self, datas):
+        inter_for = {}
+        for interface in datas['interfaces']:
+            vlan_for = datas['interfaces'][interface]['vlan']
+            inter_for[interface] = vlan_for
+
         return inter_for
